c74143f588bd622cf1b6cbb896c04c57aebbaf43
Jay Jhangiani <jaydjhangiani@gmail.com>
2021-07-18 16:29:35 +0530

    clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+
+import Authenticate from './components/Authenticate';
+import React, {useEffect, useState } from "react";
+import Navbar from './components/Navbar';
+import Dashboard from './components/Dashboard';
+
 
 function App() {
+  const [user, setUser] = useState({ haslogin: false, accessToken: "" });
+  
+  useEffect(() => {
+    if(localStorage.getItem('access-token'))
+    {
+      setUser({
+        haslogin: true,
+        accessToken: localStorage.getItem('access-token')
+      })
+    }
+  },[])
+
+console.log(user)
+
   return (
-    <div className="App">
-      
+    <div>
+      <Navbar user={user} setUser={setUser} />
+      {!user.haslogin ? <Authenticate user={user} setUser={setUser} /> : <Dashboard user={user} />}
     </div>
   );
 }
